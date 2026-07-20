84ad2b83ff2c15c7cd851b0e7f6bc75a52b0e079
lilla-st <stevinlilla@gmail.com>
2026-07-20 23:39:56 +0300

    [ADD] card UI improvements, mana UI

--- scenes/card_states/card_base_state.gd
+++ scenes/card_states/card_base_state.gd
@@ -3,13 +3,18 @@ extends CardState
 func enter() -> void:
 	if not card_ui.is_node_ready():
 		await card_ui.ready
-		
+	
+	card_ui.panel.set("theme_override_styles/panel", card_ui.BASE_STYLEBOX)	
 	card_ui.reparent_requested.emit(card_ui)
-	card_ui.color.color = Color. WEB_PURPLE
-	card_ui.state.text = "BASE"
 	card_ui.pivot_offset = Vector2.ZERO
 	
 func on_gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_mouse"):
 		card_ui.pivot_offset = card_ui.get_global_mouse_position() - card_ui.global_position
 		transition_requested.emit(self, CardState.State.CLICKED)
+
+func on_mouse_entered() -> void:
+	card_ui.panel.set("theme_override_styles/panel", card_ui.HOVER_STYLEBOX)
+	
+func on_mouse_exited() -> void:
+	card_ui.panel.set("theme_override_styles/panel", card_ui.BASE_STYLEBOX)
--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -39,3 +39,11 @@ func take_damage(damage : int) -> void:
 	
 	if stats.health <= 0:
 		queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	arrow.show()
+
+
+func _on_area_exited(area: Area2D) -> void:
+	arrow.hide()
