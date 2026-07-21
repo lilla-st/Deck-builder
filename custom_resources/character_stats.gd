b6a12a1cf2a99e1b67cd19fac35a67486242689f
lilla-st <stevinlilla@gmail.com>
2026-07-21 14:17:35 +0300

    [FIX] Debugging mana states

--- custom_resources/character_stats.gd
+++ custom_resources/character_stats.gd
@@ -4,14 +4,18 @@ extends Stats
 @export var starting_deck : CardPile
 @export var cards_per_turn: int
 @export var max_aquam: int # currently only for aquam, create rest later
-#@export var max_auram: int
-#@export var max_corpus: int
+@export var max_auram: int
+@export var max_corpus: int
 #@export var max_herbam: int
-#@export var max_ignem: int
-#@export var max_terram: int
+@export var max_ignem: int
+@export var max_terram: int
 #@export var max_mentem: int
 
 var aquam: int : set = set_aquam
+var auram: int : set = set_auram
+var corpus: int : set = set_corpus
+var ignem: int : set = set_ignem
+var terram: int : set = set_terram
 var deck: CardPile
 var discard: CardPile
 var draw_pile: CardPile
@@ -19,11 +23,31 @@ var draw_pile: CardPile
 func set_aquam(value: int) -> void:
 	aquam = value
 	stats_changed.emit()
+
+func set_auram(value: int) -> void:
+	auram = value
+	stats_changed.emit()
+	
+func set_corpus(value: int) -> void:
+	corpus = value
+	stats_changed.emit()
+	
+func set_ignem(value: int) -> void:
+	ignem = value
+	stats_changed.emit()
+	
+func set_terram(value: int) -> void:
+	terram = value
+	stats_changed.emit()
 	
 func reset_mana() -> void:
-	self.aquam = max_aquam # create for other elements
+	self.aquam = max_aquam
+	self.auram = max_auram
+	self.corpus = max_corpus
+	self.ignem = max_ignem
+	self.terram = max_terram
 	
-func can_play_card(card: Card) -> bool:
+func can_play_card(card: Card) -> bool: #additional mana required
 	return aquam >= card.cost 
 	
 func create_instance() -> Resource:
--- scenes/ui/mana_ui.gd
+++ scenes/ui/mana_ui.gd
@@ -3,7 +3,16 @@ extends Panel
 
 @export var char_stats: CharacterStats : set = _set_char_stats
 
-@onready var mana_label: Label = $ManaLabel
+@onready var aquam_label: Label = $AquamLabel
+@onready var auram_label: Label = $AuramLabel
+@onready var corpus_label: Label = $CorpusLabel
+#@onready var herbam_label: Label = $HerbamLabel
+@onready var ignem_label: Label = $IgnemLabel
+@onready var terram_label: Label = $TerramLabel
+#@onready var mentem_label: Label = $MentemLabel
+
+func _ready() -> void:
+	char_stats.aquam = char_stats.max_aquam
 
 func _set_char_stats(value: CharacterStats) -> void:
 	char_stats = value
@@ -15,4 +24,9 @@ func _set_char_stats(value: CharacterStats) -> void:
 		await ready
 	
 func _on_stats_changed() -> void:
-	mana_label.text = "%s/%s" % [char_stats.aquam, char_stats.max_aquam] #do this for all mana
+	aquam_label.text = "%s/%s" % [char_stats.aquam, char_stats.max_aquam]
+	auram_label.text = "%s/%s" % [char_stats.auram, char_stats.max_auram]
+	corpus_label.text = "%s/%s" % [char_stats.corpus, char_stats.max_corpus]
+	ignem_label.text = "%s/%s" % [char_stats.ignem, char_stats.max_ignem]
+	terram_label.text = "%s/%s" % [char_stats.terram, char_stats.max_terram]
+	
