4e92945494f7c55a06d73dd5a15a5539006f897a
lilla-st <stevinlilla@gmail.com>
2026-08-09 23:51:11 +0300

    [ADD] draw and discard logic

--- global/events.gd
+++ global/events.gd
@@ -6,3 +6,10 @@ signal card_aim_ended(card_ui: CardUI)
 signal card_played(card: Card)
 signal card_drag_started(card_ui: CardUI)
 signal card_drag_ended(card_ui: CardUI)
+signal card_tooltip_requested(icon: Texture, text: String)
+signal tooltip_hide_requested
+
+#player events
+signal player_hand_drawn
+signal player_hand_discarded
+signal player_turn_ended
